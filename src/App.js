1a465c7bfd970347a97c6271c00855d169fbbd21
sandramercadal <144231118+sandramercadal@users.noreply.github.com>
2025-09-29 16:48:07 +0100

    Updated the landing page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-     Tech Insight
+     Happy Friday!
         </p>
         <a
           className="App-link"
